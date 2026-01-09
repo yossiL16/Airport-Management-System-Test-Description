ea65f214e6a4a8d40a4b31adf5966d3077593ae3
yossiL16 <ylerer770@gmail.com>
2026-01-09 16:05:39 +0200

    Refactor VIPticket constructor to simplify parameters fix from Unnecessary inheritance

--- src/classes/VIPticket.js
+++ src/classes/VIPticket.js
@@ -1,8 +1,10 @@
 import Ticket from "./Ticket.js";
 
 export default class VIPticket extends Ticket {
-    constructor(randomTicketNumber, price, ownerName){
-        super(randomTicketNumber, price, ownerName);
+    constructor(price){
+        super(price);
+        this.randomTicketNumber = Math.floor(Math.random() * (99999 - 10000) + 10000)
+        this.ownerName = null;
         this.benefitsList = ["Free alcohol", "Free food", "Hot towels"]
     }
 }
