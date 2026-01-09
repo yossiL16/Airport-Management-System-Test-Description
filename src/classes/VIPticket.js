add7bed17a2a0026546a66125ce995809f2c5468
yossiL16 <ylerer770@gmail.com>
2026-01-09 16:07:17 +0200

    Remove unused properties from VIPticket class
    Removed random ticket number and owner name properties from VIPticket class.

--- src/classes/VIPticket.js
+++ src/classes/VIPticket.js
@@ -3,8 +3,6 @@ import Ticket from "./Ticket.js";
 export default class VIPticket extends Ticket {
     constructor(price){
         super(price);
-        this.randomTicketNumber = Math.floor(Math.random() * (99999 - 10000) + 10000)
-        this.ownerName = null;
         this.benefitsList = ["Free alcohol", "Free food", "Hot towels"]
     }
 }
